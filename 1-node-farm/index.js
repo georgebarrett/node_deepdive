0c016e09372c80516c00b7c1739b03ce5d2ad93b
George Barrett <georgebarrett@Georges-MBP.localdomain>
2023-10-24 15:52:35 +0100

    using fs to create text files

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -6,3 +6,7 @@ const fs = require('fs');
 
 const inputText = fs.readFileSync('./starter/txt/input.txt', 'utf-8');
 console.log(inputText); 
+
+const outputText = `This is what we know about the avocado: ${inputText}.\nCreated on ${Date.now()}`;
+fs.writeFileSync('./starter/txt/output.txt', outputText);
+console.log('File written');
