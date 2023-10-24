2df216feb6c9c37b0e852c3b0e93fac9d284d127
George Barrett <georgebarrett@Georges-MBP.localdomain>
2023-10-24 15:42:41 +0100

    reading a txt file using fs

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,5 +1,8 @@
 // storing the file system module in a variable that can be used 
-const fs = require('fs')
+const fs = require('fs');
 
-const hello = 'hola mundo';
-console.log(hello);
+// const hello = 'hola mundo';
+// console.log(hello);
+
+const inputText = fs.readFileSync('./starter/txt/input.txt', 'utf-8');
+console.log(inputText); 
