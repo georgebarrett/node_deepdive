6f20a6eed18991201f2522e6af34e49a409cf741
George Barrett <georgeaustinbarrett@gmail.com>
2023-10-30 16:33:37 +0000

    event loops and polling phases

--- 2-how-node-works/starter/eventLoop.js
+++ 2-how-node-works/starter/eventLoop.js
@@ -7,7 +7,12 @@ setTimeout(() => console.log('timer 1 finished'), 0);
 setImmediate(() => console.log('immediate 1 finished'));
 
 fs.readFile('test-file.txt', () => {
-    console.log('io finished')
+    console.log('io finished');
+    console.log('-----------');
+    
+    setTimeout(() => console.log('timer 2 finished'), 0);
+    setTimeout(() => console.log('timer 3 finished'), 3000);
+    setImmediate(() => console.log('immediate 2 finished'));
 });
 
 console.log('hola from the top level');
