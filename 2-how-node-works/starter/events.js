1110d8dc5106ca35ca8b9df8058516603754c972
George Barrett <georgeaustinbarrett@gmail.com>
2023-10-31 10:22:03 +0000

    more useful comments added

--- 2-how-node-works/starter/events.js
+++ 2-how-node-works/starter/events.js
@@ -4,12 +4,12 @@ const EventEmitter = require('events');
 // storing a new instance of the module in a variable 
 const myEmitter = new EventEmitter();
 
-// this is a callback action that occur once the 'buy' button has been hit
+// this is a callback action that occur once the 'buy' button has been hit (observers)
 myEmitter.on('newSale', () => {
     console.log('there was a new sale');
 });
 
-// this is a callback action that occur once the 'buy' button has been hit
+// this is a callback action that occur once the 'buy' button has been hit (observers)
 myEmitter.on('newSale', () => {
     console.log('customer name: Aphex Twin');
 });
