da6174487cd5ee3bc96bd2da5dfcfea313f5b6d8
George Barrett <georgeaustinbarrett@gmail.com>
2023-10-31 16:28:49 +0000

    practice using streams do display large pieces of data chunk by chunk. comments included

--- 2-how-node-works/starter/streams.js
+++ 2-how-node-works/starter/streams.js
@@ -3,15 +3,32 @@ const fs = require('fs');
 // short hand for creating a new server
 const server = require('http').createServer();
 
+// solution 1
+// it's important to note that this approach is only ideal for small text files
 // server.on registers a request event listener. this is emitted every time a new request comes in
 server.on('request', (req, res) => {
     // this code reads the text file in an asynchronous fashion. because it is asynchronous it won't 
     // block the execution of other code whilst the file is being read 
-    fs.readFile('test-file.txt', (err, data) => {
+    // fs.readFile('test-file.txt', (err, data) => {
         // once the file has been read or an error has occured then this callback function is executed
-        if (err) console.log(err);
+        // if (err) console.log(err);
         // this will send the data from the text file to the server
-        res.end(data);
+        // res.end(data);
+    // });
+
+    // solution 2: streams
+    // this approach streams the content of the file to client rather than storing it as a variable and sending the entire piece
+    const readable = fs.createReadStream('test-file.txt');
+    readable.on('data', chunk => {
+        // this streams/writes all the data chunk by chunk
+        res.write(chunk);   
+    });
+    // this is to handle the stream once the data is read
+    // the end event is ommitted
+    readable.on('end', () => {
+        // the response is a stream therefore the 'end' method needs to be called to signify that there is no more data
+        // nothing needs to be passed into the 'end' method because the data has been written using the 'res.write(chunk)' above
+        res.end();
     });
 });
 
