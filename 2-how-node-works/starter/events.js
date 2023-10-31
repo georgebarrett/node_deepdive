977ce5173e4413ba1e9f019d22cce7355950babe
George Barrett <georgeaustinbarrett@gmail.com>
2023-10-31 11:18:52 +0000

    value added to the emitter. the listeners can use this value inside a variable

--- 2-how-node-works/starter/events.js
+++ 2-how-node-works/starter/events.js
@@ -4,18 +4,24 @@ const EventEmitter = require('events');
 // storing a new instance of the module in a variable 
 const myEmitter = new EventEmitter();
 
-// this is a callback action that occur once the 'buy' button has been hit (observers)
+// this is a callback action that occurs once the 'buy' button has been hit (observers)
 myEmitter.on('newSale', () => {
     console.log('there was a new sale');
 });
 
-// this is a callback action that occur once the 'buy' button has been hit (observers)
+// this is a callback action that occurs once the 'buy' button has been hit (observers)
 myEmitter.on('newSale', () => {
     console.log('customer name: Aphex Twin');
 });
 
+// this listner/callback function argument takes the value from the 'emit'
+myEmitter.on('newSale', stock => {
+    console.log(`there are now ${stock} items left in stock`);
+});
+
 // emitting an event called newSale. pretend it's for a customer who has just hit the 'buy' button
-myEmitter.emit('newSale');
+// i emmitted the 9 and then the 'on' listnener can pick up the value
+myEmitter.emit('newSale', 9);
 
 
 
