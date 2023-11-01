1b67727b15f3bca5ec16525c78a1ba5faba61b65
George Barrett <georgeaustinbarrett@gmail.com>
2023-11-01 16:42:06 +0000

    practice with requiring modules that contain classes, which contain methods that can be invoked

--- 2-how-node-works/starter/modules.js
+++ 2-how-node-works/starter/modules.js
@@ -1,2 +1,10 @@
-console.log(arguments);
-console.log(require('module').wrapper);
+// console.log(arguments);
+// console.log(require('module').wrapper);
+
+// i'm storing my calculator class in a variable called X. must be capital due to being a class 
+const X = require('./testModuleOne');
+
+// a new instance of my calculator class that is stored in the X variable
+const calculator1 = new X();
+// i can now use the newly made variable and invoke the methods from calculator class (X)
+console.log(calculator1.add(2, 2));
