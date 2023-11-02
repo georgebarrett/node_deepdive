701e1ce8bae5a5f9c98fa3580e31bd3b6b6015db
George Barrett <georgeaustinbarrett@gmail.com>
2023-11-02 12:15:27 +0000

    superagent used in a different way to prevent callback hell

--- 3-asynchronous-JS/starter/index.js
+++ 3-asynchronous-JS/starter/index.js
@@ -5,9 +5,24 @@ const superagent = require('superagent');
 fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
     console.log(`breed: ${data}`);
 
+    // solution 1
+    
     // superagent module allows for get requests to api
     // without the .end method the request would not be finalised. the body refers to the random dog image
-    superagent.get(`https://dog.ceo/api/breed/${data}/images/random`).end((err, result) => {
-        console.log(result.body);
-    });
+    
+    // superagent.get(`https://dog.ceo/api/breed/${data}/images/random`).end((err, result) => {
+    //     console.log(result.body);
+    // });
+
+    // solution 2
+
+    // this solution uses the .then method which is far more in vogue and prevents callback hell
+
+    superagent
+        .get(`https://dog.ceo/api/breed/${data}/images/random`)
+        .end((err, result) => {
+            // the result is the data retrieved. the body is the random image from the api
+            // and the message refers to 'greyhound' in the dog.txt file 
+            console.log(result.body.message);
+        });
 });
