f3d1b4e7bfee757fde59c8858da5b01dfc9896c0
George Barrett <georgeaustinbarrett@gmail.com>
2023-11-02 12:01:03 +0000

    useful comments added

--- 3-asynchronous-JS/starter/index.js
+++ 3-asynchronous-JS/starter/index.js
@@ -1,9 +1,12 @@
 const fs = require('fs');
 const superagent = require('superagent');
 
+// reading file and locating it using __dirname. using the console to print the text
 fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
     console.log(`breed: ${data}`);
 
+    // superagent module allows for get requests to api
+    // without the .end method the request would not be finalised. the body refers to the random dog image
     superagent.get(`https://dog.ceo/api/breed/${data}/images/random`).end((err, result) => {
         console.log(result.body);
     });
