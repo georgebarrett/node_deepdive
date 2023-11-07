5f7bab8aadbc12ece757e59bd0928b72c6f9b597
George Barrett <georgeaustinbarrett@gmail.com>
2023-11-07 17:00:04 +0000

    post method defined. this creates a new tour. tested using postman

--- 4-natours/starter/index.js
+++ 4-natours/starter/index.js
@@ -1,7 +1,11 @@
 const fs = require('fs');
 const express = require('express');
+const { request } = require('http');
 
 const app = express();
+
+// express.json is middleware so that data from the client side can be attached to request objects
+app.use(express.json());
 
 // once the home page url has been hit. the callback function will fire
 // app.get('/', (req, res) => {
@@ -26,12 +30,22 @@ app.get('/api/v1/tours', (req, res) => {
     res.status(200).json({
         // the status of the get request
         status: 'success',
+        // providing the length of the tours makes it easy for a programmer to see the quantity
+        results: tours.length, 
         // data: is an envelope that contains the actual data response object
         data: {
             // key/url endpoint and value/tours variable
             tours: tours
         }
     });
+});
+
+// the request object is what holds all the data that is been posted
+app.post('/api/v1/tours', (req, res) => {
+    // body refers to the object data that is attached to the request
+    console.log(req.body);
+    // this is sending back a response
+    res.send('response');
 });
 
 const port = 3000;
