856c850c6bf25d081a58d6c65e742e4e16679492
George Barrett <georgeaustinbarrett@gmail.com>
2023-11-08 16:08:07 +0000

    defined two middleware functions. one displays a string to the console when a request is made and the other attaches the time and date to a request

--- 4-natours/starter/index.js
+++ 4-natours/starter/index.js
@@ -8,6 +8,20 @@ const app = express();
 // express.json is middleware so that data from the client side can be attached to request objects
 app.use(express.json());
 
+// by passing next, express knows i am defining a middle ware function
+// middleware functions must be above any http response or they won't be included in the cycle
+app.use((req, res, next) => {
+    console.log('middleware');
+    // always use next in middleware functions otherwise the request response cycle would get stuck
+    next();
+});
+
+app.use((req, res, next) => {
+    // this is attaching a date to the request. .toISOString converts the date into a nice string
+    req.requestTime = new Date().toISOString();
+    next();
+});
+
 // by adding v1, i can work on v2 without messing with the original request setup
 app.get('/api/v1/tours', tourController.getAllTours);
 
