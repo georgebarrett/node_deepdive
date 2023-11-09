ff39398888f046c427b8d600c24ca348a4df55f2
George Barrett <georgeaustinbarrett@gmail.com>
2023-11-09 16:32:34 +0000

    middleware function that displays the requested tour id in the console

--- 4-natours/starter/routes/tourRoutes.js
+++ 4-natours/starter/routes/tourRoutes.js
@@ -3,6 +3,13 @@ const toursController = require('../controllers/toursController');
 
 const routes = express();
 
+// the value holds the id parameter
+// this middleware function only applies to the tours 
+routes.param('id', (req, res, next, value) => {
+    console.log(`tour id is: ${value}`);
+    next();
+});
+
 routes
     .route('/')
     .get(toursController.getAllTours)
