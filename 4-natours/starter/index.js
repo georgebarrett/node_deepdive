f2d3659c30476d6ab371983717531e82075f96ff
George Barrett <georgeaustinbarrett@gmail.com>
2023-11-10 16:46:39 +0000

    middleware function defined that allows express to read static files

--- 4-natours/starter/index.js
+++ 4-natours/starter/index.js
@@ -14,6 +14,10 @@ app.use(morgan('dev'));
 // express.json is middleware so that data from the client side can be attached to request objects
 app.use(express.json());
 
+// this is for being able to access static files
+// it also sets the public folder to the route
+app.use(express.static(`${__dirname}/public`));
+
 // by passing next, express knows i am defining a middle ware function
 // middleware functions must be above any http response or they won't be included in the cycle
 app.use((req, res, next) => {
