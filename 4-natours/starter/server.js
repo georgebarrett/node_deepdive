c1f819844e28dc79e9b0b94393364882dee2f822
George Barrett <georgeaustinbarrett@gmail.com>
2023-11-14 13:00:21 +0000

    basic tour schema defined

--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -5,8 +5,10 @@ dotenv.config({ path: './config.env' });
 
 const app = require('./index');
 
+// this is accessing code from the .env vile
 const db = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
 
+// this function connects to my mongodb database using mongoose
 const connectdb = async () => {
     try {
         await mongoose.connect(db);
@@ -15,8 +17,29 @@ const connectdb = async () => {
         console.error('database connection failed', error);
     }
 };
-
+// classic invoking the function
 connectdb();
+
+// outlining the schema and storing it in a variable
+const tourSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        // the error handling is in the string. square braces essential
+        required: [true, 'a tour must have a name'],
+        // true ensures that no two documents can have the same name
+        unique: true
+    },
+    rating: {
+        type: Number,
+        // default means that if no rating is left by the user, 4.5 will automatically be assigned
+        default: 4.5
+
+    },
+    price: {
+        type: Number,
+        required: [true, 'a tour must have a price']
+    }
+});
 
 // console.log(process.env);
 
