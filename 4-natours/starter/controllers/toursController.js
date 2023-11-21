c79623e372d5a2b26dd5bcc8cc4e1438030497e8
George Barrett <georgeaustinbarrett@gmail.com>
2023-11-21 10:03:06 +0000

    comments organised

--- 4-natours/starter/controllers/toursController.js
+++ 4-natours/starter/controllers/toursController.js
@@ -1,46 +1,6 @@
 const fs = require('fs');
 const Tour = require('../models/tourModel');
 
-// USE OF JSON.PARSE
-
-// JSON.parse means the json from the file will automatically be converted into a javascript object
-// const tours = JSON.parse(
-//     fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
-// );
-
-
-// USE OF MIDDLEWARE (i don't need a checkId function because mongodb automatically asigns an id)
-
-// this middleware function is following best express practice.
-// creating an error handling function and storing it in a variable and then adding this to each function
-// would not be following the middleware pipeline convention
-// const checkId = (req, res, next, value) => {
-//     console.log(`tour id is: ${value}`)
-//     // * 1 will convert a string into an integer
-//     if (req.params.id * 1 > tours.length) {
-//         // without the return node would continue running the code and hit the next function
-//         return res.status(404).json({
-//             status: 'fail',
-//             message: 'invalid id'
-//         });
-//     }
-//     next();
-// };
-
-// I don't need this middleware function because the body of the data is checked in the schema
-// this great middleware function checks the body of the request for certain things before next()
-// the point of middleware functions is to remove all aspects of the main functions leaving only the core
-// const checkBody = (req, res, next) => {
-//     if (!req.body.name || !req.body.price) {
-//         // 400 is bad request
-//         return res.status(400).json({
-//             status: 'fail',
-//             message: 'invalid name and/or price'
-//         });
-//     }
-//     next();
-// };
-
 aliasTopFiveCheapestTours = (req, res, next) => {
     // this is a url query in middleware format that prefills parts of the query object
     req.query.limit = '5';
@@ -58,33 +18,23 @@ class APIFeatures {
 
     filter() {
         // {...} = js spread operator. creating a new object from the properties of req.query
-        // queryObject has the same properties as req.query
+        // queryObject has the same properties as this.query
         // spread operators are used to modify an object but preserve the original. this allows for experiments
         const queryObject = {...this.queryString};
-        // these are url params that I want to exclude from the request
+        // these are url params that I want to exclude from this.queryString
         const excludedFields = ['page', 'sort', 'limit', 'fields'];
         // this is the process for excluding the params
         excludedFields.forEach(element => delete queryObject[element]);
-        // this logs my url query to the console
-        // console.log(req.query, queryObject);
-
-        // variable that stores - Tour model with the find method. queryObject is passed in which is the copy
-        // of the req.query object { difficulty: 'easy' } 
-        // const query = Tour.find(queryObject);
 
         // 2. ADVANCED FILTERING
 
         // this variable can be redfined - JSON.stringify converts the queryObject to a JSON string
         let queryString = JSON.stringify(queryObject);
-        // queryString is redifined - the replace functions adds a $ to the maths quieries
+        // queryString is redifined - the replace functions adds a $ to the maths queries
         // the 'g' ensures the $ is added to all the occurrences. not just the first occurence 
         queryString = queryString.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
         
         // JSON.parse coverts the json string back into a javascript object
-        // console.log(JSON.parse(queryString));
-        
-        // this is using the model method to find the documents that match the argument criteria
-        // let query = Tour.find(JSON.parse(queryString));
         this.query = this.query.find(JSON.parse(queryString));
         // 'this' refers to the entire object
         return this;
@@ -98,7 +48,6 @@ class APIFeatures {
             // this modifies the existing query pattern to include a sorting method
             this.query = this.query.sort(sortBy);
         } else {
-            // this is for if a user does not specify a sort field
             // creating a default sorting method (descending order)
             this.query = this.query.sort('-createdAt');
         }
@@ -115,8 +64,7 @@ class APIFeatures {
             // this modifies the existing mongoose query so that it only includes the fields that are defined
             this.query = this.query.select(fields);
         } else {
-            // by default i am only excluding the __v field (this is a special field for mongoose)
-            // the - means exclude
+            // by default i am only excluding the __v field (this is a special field for mongoose) -means exclude
             this.query = this.query.select('-__v')
         }
         return this;
@@ -171,8 +119,14 @@ const getAllTours = async (req, res) => {
                 tours: tours
             }
         });
-
-        // ALTERNATIVE METHOD
+    } catch (error) {
+        res.status(404).json({
+            status: 'failed',
+            message: error.message
+        });
+    }
+   
+    // ALTERNATIVE METHOD
 
         // await until all the tours are found and then return them
         // the find method returns an array containing javascript objects
@@ -191,12 +145,6 @@ const getAllTours = async (req, res) => {
         //         tours: allTours
         //     }
         // });
-    } catch (error) {
-        res.status(404).json({
-            status: 'failed',
-            message: error.message
-        });
-    }
 };
 
 const getTourById = async (req, res) => {
@@ -253,34 +201,6 @@ const createTour = async (req, res) => {
             message: 'incomplete tour data'
         });   
     }
-
-    // the request object refers to the data that is being posted
-    // body refers to the object data that is attached to the request
-    // console.log(req.body);
-
-    // because there is no database and the data is coming from a regular .js file
-    // use the [tours.length - 1] to find the last posted tour object
-    // the .id accesses the id of the last tour object
-    // the + 1 generates a 
-    // const newId = tours[tours.length - 1].id + 1
-
-    // object.assign allows for creation of a new object by merging two existing objects together
-    // the new object tour has been created and stored in newTour
-    // const newTour = Object.assign({ id: newId }, req.body);
-
-    // the tours array now has the new tour pushed in with its id
-    // tours.push(newTour);
-
-    // writeFileSync would block the event loop!
-    // fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
-    //     status 201 means created
-    //     res.status(201).json({
-    //         status: 'success',
-    //         data: {
-    //             tour: newTour
-    //         }
-    //     });
-    // });
 };
 
 const updateTour = async (req, res) => {
@@ -323,13 +243,6 @@ const deleteTour = async (req, res) => {
             message: 'tour not deleted'
         });
     }
-    
-    // 204 means 'no content'
-    // res.status(204).json({
-    //     status: 'success',
-    //     null proves the data i deleted no longer exists
-    //     data: null
-    // });
 };
 
 module.exports = {
@@ -340,3 +253,77 @@ module.exports = {
     deleteTour,
     aliasTopFiveCheapestTours
 };
+
+
+// WRITING TO FILES
+
+// the request object refers to the data that is being posted
+// body refers to the object data that is attached to the request
+// console.log(req.body);
+
+// because there is no database and the data is coming from a regular .js file
+// use the [tours.length - 1] to find the last posted tour object
+// the .id accesses the id of the last tour object
+// the + 1 generates a 
+// const newId = tours[tours.length - 1].id + 1
+
+// object.assign allows for creation of a new object by merging two existing objects together
+// the new object tour has been created and stored in newTour
+// const newTour = Object.assign({ id: newId }, req.body);
+
+// the tours array now has the new tour pushed in with its id
+// tours.push(newTour);
+
+// writeFileSync would block the event loop!
+// fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
+//     status 201 means created
+//     res.status(201).json({
+//         status: 'success',
+//         data: {
+//             tour: newTour
+//         }
+//     });
+// });
+
+
+// USE OF JSON.PARSE
+
+// JSON.parse means the json from the file will automatically be converted into a javascript object
+// const tours = JSON.parse(
+//     fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
+// );
+
+
+// USEFUL CODE
+
+// USE OF MIDDLEWARE (i don't need a checkId function because mongodb automatically asigns an id)
+
+// this middleware function is following best express practice.
+// creating an error handling function and storing it in a variable and then adding this to each function
+// would not be following the middleware pipeline convention
+// const checkId = (req, res, next, value) => {
+//     console.log(`tour id is: ${value}`)
+//     // * 1 will convert a string into an integer
+//     if (req.params.id * 1 > tours.length) {
+//         // without the return node would continue running the code and hit the next function
+//         return res.status(404).json({
+//             status: 'fail',
+//             message: 'invalid id'
+//         });
+//     }
+//     next();
+// };
+
+// I don't need this middleware function because the body of the data is checked in the schema
+// this great middleware function checks the body of the request for certain things before next()
+// the point of middleware functions is to remove all aspects of the main functions leaving only the core
+// const checkBody = (req, res, next) => {
+//     if (!req.body.name || !req.body.price) {
+//         // 400 is bad request
+//         return res.status(400).json({
+//             status: 'fail',
+//             message: 'invalid name and/or price'
+//         });
+//     }
+//     next();
+// };
