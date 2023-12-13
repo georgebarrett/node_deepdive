c7ef521c3b88ecbd99ffa837100bc0237635a13e
George Barrett <georgeaustinbarrett@gmail.com>
2023-12-13 09:08:04 +0000

    error controller modified so that different errors are displayed depending on the environment production/development

--- 4-natours/starter/controllers/errorController.js
+++ 4-natours/starter/controllers/errorController.js
@@ -1,10 +1,27 @@
+const sendDevError = (error, res) => {
+    res.status(error.statusCode).json({
+        status: error.status,
+        error: error,
+        message: error.message,
+        stack: error.stack
+    });
+};
+
+const sendProdError = (error, res) => {
+    res.status(error.statusCode).json({
+        status: error.status,
+        message: error.message,
+    });
+};
+
 module.exports = (error, req, res, next) => {
     
     error.statusCode = error.statusCode || 500;
     error.status = error.status || 'error'
-    
-    res.status(error.statusCode).json({
-        status: error.status,
-        message: error.message
-    });
+
+    if (process.env.NODE_ENV === 'development') {
+        sendDevError(error, res);
+    } else if (process.env.NODE_ENV === 'production') {
+        sendProdError(error, res);
+    }  
 };
