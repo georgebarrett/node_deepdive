61fc9980257b26bdc4c4fc943dbf6e774b756eda
George Barrett <georgeaustinbarrett@gmail.com>
2023-12-18 12:40:22 +0000

    schema defined for the user model

---
+++ 4-natours/starter/models/userModel.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const userSchema = new mongoose.schema({
+    name: {
+        type: String,
+        required: [true, 'a user must have a name'],
+        trim: true,
+        minLength: [1, 'a name must have more than one character']
+    },
+    email: {
+        type: String,
+        required: [true, 'please provide your email'],
+        unique: true,
+        lowercase: true,
+        validate: [validator.isEmail, 'please provide a valid email']
+    },
+    photo: String,
+    password: {
+        type: String,
+        required: [true, 'please provide a password'],
+        minLength: 8
+    },
+    passwordConfirmation: {
+        type: String,
+        required: [true, 'please confirm your password']
+    }
+});
