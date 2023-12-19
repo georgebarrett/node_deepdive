c0fd888e5d8f26d4a3639b8ad2d8b9a9eaf5b9e4
George Barrett <georgeaustinbarrett@gmail.com>
2023-12-19 10:58:23 +0000

    login function defined. currently it finds a user in the mongodb database by their email address and includes their password in the result

--- 4-natours/starter/controllers/authController.js
+++ 4-natours/starter/controllers/authController.js
@@ -1,6 +1,7 @@
 const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
 const catchAsyncErrors = require('../utils/catchAsyncError');
+const AppError = require('../utils/appError');
 
 const signup = catchAsyncErrors(async (req, res, next) => {
     const newUser = await User.create({
@@ -23,6 +24,25 @@ const signup = catchAsyncErrors(async (req, res, next) => {
     });
 });
 
+const login = catchAsyncErrors(async (req, res, next) => {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+        return next(new AppError('please provide email and password', 400));
+    }
+
+    const user = await User.findOne({email: email}).select('+password');
+
+    console.log(user);
+
+    const token = '';
+    res.status(200).json({
+        status: 'success',
+        token
+    });
+});
+
 module.exports = {
-    signup
+    signup,
+    login
 };
