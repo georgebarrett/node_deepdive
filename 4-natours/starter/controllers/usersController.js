8555ce6084a8cb54340598a20aa0952954e6834e
George Barrett <georgeaustinbarrett@gmail.com>
2023-12-19 10:26:29 +0000

    getALLUsers function defined. this gets all the users. the user model prevents password leakage when all the user info is retrieved

--- 4-natours/starter/controllers/usersController.js
+++ 4-natours/starter/controllers/usersController.js
@@ -1,17 +1,25 @@
 const fs = require('fs');
+const User = require('../models/userModel');
+const catchAsyncError = require('../utils/catchAsyncError');
+const { features } = require('process');
 
 // JSON.parse means the json from the file will automatically be converted into a javascript object
 const tours = JSON.parse(
     fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
 );
 
-const getAllUsers = (req, res) => {
+const getAllUsers = catchAsyncError(async (req, res, next) => {
+    const users = await User.find();
+
     // 500 = internal server error
-    res.status(500).json({
-        status: 'error',
-        message: 'route not yet defined'
+    res.status(200).json({
+        status: 'success',
+        results: users.length,
+        data: {
+            users: users
+        }
     });
-};
+});
 
 const getUserById = (req, res) => {
     res.status(500).json({
