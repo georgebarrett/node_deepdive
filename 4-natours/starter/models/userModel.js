ebda3264dd0859fd07283a15e13564b4b41f7b49
George Barrett <georgeaustinbarrett@gmail.com>
2023-12-19 12:35:14 +0000

    instance method defined that uses a bcrypt in-built method to compare encrypted passwords with non-encrypted ones

--- 4-natours/starter/models/userModel.js
+++ 4-natours/starter/models/userModel.js
@@ -46,6 +46,14 @@ userSchema.pre('save', async function(next) {
 });
 
 
+// instance method - method that available on all documents of a certain collection
+
+userSchema.methods.correctPassword = async function(candidatePassword, userPassword) {
+    return await bcrypt.compare(candidatePassword, userPassword);
+};
+
+
+
 const User = mongoose.model('User', userSchema);
 
 module.exports = User;
