024c195322b656e01c71f437f25f3367ef4b9d70
George Barrett <georgeaustinbarrett@gmail.com>
2024-01-30 13:48:45 +0000

    function defined that is responsible for sending emails to users. an example of this would be a password reset email to a user via their email

---
+++ 4-natours/starter/utils/email.js
@@ -0,0 +1,42 @@
+const nodemailer = require('nodemailer');
+
+const sendEmail = async (options) => {
+    
+    const transporter = nodemailer.createTransport({
+        host: process.env.EMAIL_HOST,
+        port: process.env.EMAIL_PORT,
+        auth: {
+            user: process.env.EMAIL_USERNAME,
+            password: process.env.EMAIL_PASSWORD   
+        }
+    });
+
+    const mailOptions = {
+        from: 'Aphex Twin <george@george.com>',
+        to: options.email,
+        subject: options.subject,
+        text: options.message
+    };
+
+    await transporter.sendMail(mailOptions);
+};
+
+module.exports = sendEmail;
+
+
+
+
+
+
+
+
+
+
+// FOR GMAIL
+    // const transporter = nodemailer.createTransport({
+        // service: 'Gmail',
+        // auth: {
+            // user: process.env.EMAIL_USERNAME
+            // password: process.env.EMAIL_PASSWORD   
+        // }
+    // })
