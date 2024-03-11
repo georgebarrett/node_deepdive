5ad78b65f9cbc01f98bdec5e9c34d28d1ac665a8
George Barrett <georgeaustinbarrett@gmail.com>
2024-03-11 10:42:40 +0000

    code tidy

--- 4-natours/starter/routes/userRoutes.js
+++ 4-natours/starter/routes/userRoutes.js
@@ -9,11 +9,7 @@ routes
   .post('/login', authcontroller.login)
   .post('/forgotPassword', authcontroller.forgotPassword)
   .patch('/resetPassword/:token', authcontroller.resetPassword)
-  .patch(
-    '/updateMyPassword',
-    authcontroller.protect,
-    authcontroller.updatePassword,
-  )
+  .patch('/updateMyPassword',authcontroller.protect,authcontroller.updatePassword,)
   .patch('/updateMe', authcontroller.protect, usersController.updateMe);
 
 routes
