fed056ab85b5d1bfafe146f3bd93ed0cf0fbcdf4
George Barrett <georgeaustinbarrett@gmail.com>
2024-04-10 12:56:19 +0100

    filterObject utility function defined that filters an object by property names. updateMe middleware function defined to allow users to update permitted parts of their account

--- 4-natours/starter/controllers/usersController.js
+++ 4-natours/starter/controllers/usersController.js
@@ -4,6 +4,17 @@ const AppError = require('../utils/appError');
 const catchAsyncError = require('../utils/catchAsyncError');
 const { features } = require('process');
 
+const filterObject = (object, ...allowedFields) => {
+  const newObject = {};
+  // looping through an object in javascript
+  // returns an array containing all the key names
+  // can then use forEach to loop through them
+  Object.keys(object).forEach(el => {
+    if (allowedFields.includes(el)) newObject[el] = object[el];
+  });
+  return newObject;
+}
+
 // JSON.parse means the json from the file will automatically be converted into a javascript object
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`),
@@ -23,7 +34,7 @@ const getAllUsers = catchAsyncError(async (req, res, next) => {
 });
 
 // updating the currently athenticated user
-const updateMe = (req, res, next) => {
+const updateMe = catchAsyncError(async (req, res, next) => {
   if (req.body.password || req.body.passwordConfirmation) {
     return next(
       new AppError(
@@ -32,11 +43,18 @@ const updateMe = (req, res, next) => {
       ),
     );
   }
+  // filtering out unwanted field names
+  const filteredBody = filterObject(req.body, 'name', 'email');
+  // update user document
+  const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {new: true, runValidators: true});
 
   res.status(200).json({
     status: 'success',
+    data: {
+      user: updatedUser
+    }
   });
-};
+});
 
 const getUserById = (req, res) => {
   res.status(500).json({
