11856422223567fd47f051ea87a6be08716f4a54
George Barrett <georgeaustinbarrett@gmail.com>
2024-04-24 12:09:38 +0100

    new nested route defined that handles the reviews that are attached to a tour

--- 4-natours/starter/routes/tourRoutes.js
+++ 4-natours/starter/routes/tourRoutes.js
@@ -1,32 +1,37 @@
 const express = require('express');
 const toursController = require('../controllers/toursController');
 const authController = require('../controllers/authController');
+const reviewController = require('../controllers/reviewController');
 
 const routes = express();
 
 routes
     .route('/top-5-cheapest')
     // chaining middleware pipelines. the middleware function acts as an aggregate pipeline
-    .get(toursController.aliasTopFiveCheapestTours, toursController.getAllTours)
+    .get(toursController.aliasTopFiveCheapestTours, toursController.getAllTours);
 
 routes
     .route('/tour-stats')
-    .get(toursController.getTourStats)
+    .get(toursController.getTourStats);
 
 routes
     .route('/monthly-plan/:year')
-    .get(toursController.getMonthlyPlan)
+    .get(toursController.getMonthlyPlan);
 
 routes
     .route('/')
     .get(authController.protect, toursController.getAllTours)
-    .post(toursController.createTour)
+    .post(toursController.createTour);
 
 routes
     // adding :id creates a variable in the url that can store an id integer
     .route('/:id')
     .get(toursController.getTourById)
     .patch(toursController.updateTour)
-    .delete(authController.protect, authController.restrictTo('admin', 'lead-guide'), toursController.deleteTour)
+    .delete(authController.protect, authController.restrictTo('admin', 'lead-guide'), toursController.deleteTour);
+
+routes
+    .route('/:tourId/reviews')
+    .post(authController.protect, authController.restrictTo('users'), reviewController.createReview);
 
 module.exports = routes;
