acf9b6d82b83b57c154e46493cba000014a0f5d9
George Barrett <georgeaustinbarrett@gmail.com>
2024-04-24 15:24:06 +0100

    mergeParams set to true in express.router object. now anything to do with reviews in the tour router will be funnelled through to this review router

--- 4-natours/starter/routes/reviewRoutes.js
+++ 4-natours/starter/routes/reviewRoutes.js
@@ -2,7 +2,13 @@ const express = require('express');
 const reviewController = require('../controllers/reviewController');
 const authController = require('../controllers/authController');
 
-const routes = express.Router();
+const routes = express.Router({ mergeParams: true });
+
+// mergeParams 
+// POST /tour/dh4756heu848/reviews
+// POST /tour/reviews
+// both of these routes will be re-routed to the router below using mergeParams
+// routes.use('/:tourId/reviews', reviewRouter); - this line is logic behing the re-routing and is in tourRoutes.js
 
 routes
     .route('/')
