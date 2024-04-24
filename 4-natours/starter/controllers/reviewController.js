59bed76684b14fa1fe100daca869460cd9b7b23b
George Barrett <georgeaustinbarrett@gmail.com>
2024-04-24 12:37:52 +0100

    createReview function enhanced so that the tour and user can be found if they are not directly in the request.body

--- 4-natours/starter/controllers/reviewController.js
+++ 4-natours/starter/controllers/reviewController.js
@@ -14,6 +14,11 @@ const getAllReviews = catchAsyncErrors(async (req, res, next) => {
 });
 
 const createReview = catchAsyncErrors(async (req, res, next) => {
+    // these ifs define the tour and user if they are not present in the request body
+    // if the tour id and body have not been specified, capture them in the url params
+    if (!req.body.tour) req.body.tour = req.params.tourId;
+    // req.user comes from the protect middleware
+    if (!req.body.user) req.body.user = req.user.id;
     const newReview = await Review.create(req.body);
 
     res.status(201).json({
