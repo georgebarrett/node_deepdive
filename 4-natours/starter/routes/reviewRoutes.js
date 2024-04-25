ada587b0a99a1c5f5b466f43a90822f0a0d2b36e
George Barrett <georgeaustinbarrett@gmail.com>
2024-04-25 12:15:43 +0100

    route defined for deleting a specific tour using its id in the params

--- 4-natours/starter/routes/reviewRoutes.js
+++ 4-natours/starter/routes/reviewRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const reviewController = require('../controllers/reviewController');
 const authController = require('../controllers/authController');
+const crudFactory = require('../controllers/crudFactory');
 
 const routes = express.Router({ mergeParams: true });
 
@@ -13,6 +14,11 @@ const routes = express.Router({ mergeParams: true });
 routes
     .route('/')
     .get(reviewController.getAllReviews)
-    .post(authController.protect, authController.restrictTo('user'), reviewController.createReview)
+    .post(authController.protect, authController.restrictTo('user'), reviewController.createReview);
+
+routes
+    .route('/:id')
+    .delete(reviewController.deleteReview);
+    // .delete(authController.protect, authController.restrictTo('admin'), crudFactory.deleteOne);
 
 module.exports = routes;
