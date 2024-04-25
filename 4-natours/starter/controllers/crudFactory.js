dc83220ac672058c7e3449d17dc733b612381899
George Barrett <georgeaustinbarrett@gmail.com>
2024-04-25 16:03:06 +0100

    global crud handler function defined for updating something using its id

--- 4-natours/starter/controllers/crudFactory.js
+++ 4-natours/starter/controllers/crudFactory.js
@@ -2,6 +2,24 @@ const { Model } = require('mongoose');
 const catchAsyncError = require('../utils/catchAsyncError');
 const AppError = require('../utils/appError');
 
+const updateOne = Model => catchAsyncError(async (req, res, next) => {
+    const document = await Model.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+        runValidators: true
+    });
+
+    if (!document) {
+        return next(new AppError('no document found with that id', 404));
+    }
+
+    res.status(200).json({
+        status: 'success',
+        message: 'document updated',
+        data: {
+            data: document
+        }
+    });
+});
 
 const deleteOne = Model => catchAsyncError(async (req, res, next) => {
     const document = await Model.findByIdAndDelete(req.params.id);
@@ -18,5 +36,6 @@ const deleteOne = Model => catchAsyncError(async (req, res, next) => {
 });
 
 module.exports = {
+    updateOne,
     deleteOne
 };
