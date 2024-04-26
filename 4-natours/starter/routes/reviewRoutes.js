f8b8e67ce1983160f593df9a87c859ee346b8e06
George Barrett <georgeaustinbarrett@gmail.com>
2024-04-26 16:54:03 +0100

    new route defined for getting the data of a single review by using its id

--- 4-natours/starter/routes/reviewRoutes.js
+++ 4-natours/starter/routes/reviewRoutes.js
@@ -23,6 +23,7 @@ routes
 
 routes
     .route('/:id')
+    .get(reviewController.getReviewById)
     .patch(reviewController.updateReview)
     .delete(reviewController.deleteReview);
     // .delete(authController.protect, authController.restrictTo('admin'), crudFactory.deleteOne);
