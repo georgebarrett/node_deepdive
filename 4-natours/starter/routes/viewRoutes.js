3bebd0dec7ad49902ef31f706cbf748b92d0fea1
George Barrett <georgeaustinbarrett@gmail.com>
2024-05-08 16:00:09 +0100

    file created to contain all the GET routes needed for displaying the web page

---
+++ 4-natours/starter/routes/viewRoutes.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const { route } = require('./reviewRoutes');
+
+const routes = express.Router();
+
+routes.get('/', (req, res) => {
+    res.status(200).render('base', {
+        tour: 'The Forest Hiker',
+        user: 'George'
+    });
+});
+
+routes.get('/overview', (req, res) => {
+    res.status(200).render('overview', {
+        title: 'All Tours'
+    });
+});
+
+routes.get('/tour', (req, res) => {
+    res.status(200).render('tour', {
+        title: 'The Star Gazer Tour'
+    });
+});
+
+module.exports = routes;
