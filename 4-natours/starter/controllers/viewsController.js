b1ddeba2dd2e36fbacef9e7853df44b51dc65530
George Barrett <georgeaustinbarrett@gmail.com>
2024-05-16 16:59:45 +0100

    GET individual tour method defined

--- 4-natours/starter/controllers/viewsController.js
+++ 4-natours/starter/controllers/viewsController.js
@@ -12,11 +12,17 @@ const getOverview = catchAsync(async (req, res, next) => {
     });
 });
 
-const getTour = (req, res) => {
+const getTour = catchAsync(async (req, res) => {
+    const tour = await Tour.findOne({ slug: req.params.slug }).populate({
+        path: 'reviews',
+        fields: 'review rating user'
+    })
+    
     res.status(200).render('tour', {
-        title: 'The Shoe Gazer Tour'
+        title: 'Tour Details',
+        tour: tour
     });
-};
+});
 
 module.exports = {
     getOverview,
