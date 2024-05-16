b860b203258a2968901bdc975d239554ddef5a47
George Barrett <georgeaustinbarrett@gmail.com>
2024-05-16 13:36:35 +0100

    getOverview function now finds all the data from the model/db and returns it in an http response object

--- 4-natours/starter/controllers/viewsController.js
+++ 4-natours/starter/controllers/viewsController.js
@@ -1,8 +1,16 @@
-const getOverview = (req, res) => {
+const Tour = require('../models/tourModel');
+const catchAsync = require('../utils/catchAsyncError');
+
+const getOverview = catchAsync(async (req, res, next) => {
+    // fetching all the tour data from the database
+    const tours = await Tour.find();
+    
+    // all the tour data is passed into the response object
     res.status(200).render('overview', {
-        title: 'All Tours'
+        title: 'All Tours',
+        tours: tours
     });
-};
+});
 
 const getTour = (req, res) => {
     res.status(200).render('tour', {
