6ff6c176d696f5bf3905343472ee9e64c30f2818
George Barrett <georgeaustinbarrett@gmail.com>
2024-05-16 10:51:05 +0100

    GET route defined that takes the user to the homepage that is rendered using pug. the homepage file is called base.pug

--- 4-natours/starter/index.js
+++ 4-natours/starter/index.js
@@ -83,6 +83,10 @@ app.use((req, res, next) => {
     next();
 });
 
+app.get('/', (req, res) => {
+    res.status(200).render('base');
+});
+
 // adding v1 is a form of version control
 app.use('/api/v1/tours', tourRoutes);
 app.use('/api/v1/users', userRoutes);
