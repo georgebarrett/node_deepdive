b3992e01dff87f1bde7b01268b6ee51730b6d558
George Barrett <georgeaustinbarrett@gmail.com>
2024-05-16 13:25:05 +0100

    viewsController imported. functions from that controller utilised

--- 4-natours/starter/routes/viewRoutes.js
+++ 4-natours/starter/routes/viewRoutes.js
@@ -1,24 +1,9 @@
 const express = require('express');
+const viewsController = require('../controllers/viewsController');
 
 const routes = express.Router();
 
-routes.get('/', (req, res) => {
-    res.status(200).render('base', {
-        tour: 'The Shoe Gazer',
-        user: 'George'
-    });
-});
-
-routes.get('/overview', (req, res) => {
-    res.status(200).render('overview', {
-        title: 'All Tours'
-    });
-});
-
-routes.get('/tour', (req, res) => {
-    res.status(200).render('tour', {
-        title: 'The Shoe Gazer Tour'
-    });
-});
+routes.get('/', viewsController.getOverview);
+routes.get('/tour', viewsController.getTour);
 
 module.exports = routes;
