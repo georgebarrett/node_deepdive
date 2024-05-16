b4e7587a0d6e002024a961d27aa438a56a7a3faf
George Barrett <georgeaustinbarrett@gmail.com>
2024-05-16 17:01:07 +0100

    title changed to humour myself

--- 4-natours/starter/controllers/viewsController.js
+++ 4-natours/starter/controllers/viewsController.js
@@ -19,7 +19,7 @@ const getTour = catchAsync(async (req, res) => {
     })
     
     res.status(200).render('tour', {
-        title: 'Tour Details',
+        title: 'The Shoe Gazer',
         tour: tour
     });
 });
