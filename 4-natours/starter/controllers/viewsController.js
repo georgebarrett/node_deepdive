301e7877926d99576f99b31cd737c6726c63c4b3
George Barrett <georgeaustinbarrett@gmail.com>
2024-05-29 13:44:46 +0100

    getTour function now renders the real title of the tour in the browser

--- 4-natours/starter/controllers/viewsController.js
+++ 4-natours/starter/controllers/viewsController.js
@@ -19,7 +19,7 @@ const getTour = catchAsync(async (req, res) => {
     })
     
     res.status(200).render('tour', {
-        title: 'The Shoe Gazer',
+        title: `${tour.name} Tour`,
         tour: tour
     });
 });
