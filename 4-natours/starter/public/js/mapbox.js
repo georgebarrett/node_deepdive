21b3f6647e536515162d001a0302697d69cc47bd
George Barrett <georgeaustinbarrett@gmail.com>
2024-05-30 17:02:42 +0100

    this file is responsible for the javascript functionality behind the mapbox map in /tour

--- 4-natours/starter/public/js/mapbox.js
+++ 4-natours/starter/public/js/mapbox.js
@@ -1,4 +1,44 @@
 /* eslint-disabled */
 
+console.log('client says hi');
+const locations = JSON.parse(document.getElementById('map').dataset.locations);
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiZ2VvcmdlYmFycmV0dDg3OSIsImEiOiJjbHd0OTI2Y3MwMWFjMmxzZ2x0cnl5eTZjIn0.RYXyzymRdX6EHEFH-LlnIw'
+    const map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/streets-v9'
+    })
+
 // console.log('client says hi');
-// const locations = JSON.parse(document.getElementById('map').dataset.locations);
+// document.addEventListener('DOMContentLoaded', () => {
+//     const mapElement = document.getElementById('map');
+//     if (mapElement) {
+//         console.log('Map element found');
+//         const locationsData = mapElement.dataset.locations;
+//         if (locationsData) {
+//             const locations = JSON.parse(locationsData);
+
+//             // Ensure Mapbox GL JS library is loaded
+//             if (typeof mapboxgl !== 'undefined') {
+//                 console.log('Mapbox GL JS library is loaded.');
+//                 mapboxgl.accessToken = 'pk.eyJ1IjoiZ2VvcmdlYmFycmV0dDg3OSIsImEiOiJjbHd0OTI2Y3MwMWFjMmxzZ2x0cnl5eTZjIn0.RYXyzymRdX6EHEFH-LlnIw';
+//                 const map = new mapboxgl.Map({
+//                     container: 'map',
+//                     style: 'mapbox://styles/mapbox/streets-v9'
+//                 });
+
+//                 locations.forEach(location => {
+//                     new mapboxgl.Marker()
+//                         .setLngLat(location.coordinates)
+//                         .addTo(map);
+//                 });
+//             } else {
+//                 console.error('Mapbox GL JS library is not loaded.');
+//             }
+//         } else {
+//             console.error('No locations data found on the map element.');
+//         }
+//     } else {
+//         console.error('Map element not found.');
+//     }
+// });
