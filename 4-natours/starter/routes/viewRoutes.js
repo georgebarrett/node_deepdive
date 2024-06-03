aec5023dbd5da688c01682b223abab1aec9f6253
George Barrett <georgeaustinbarrett@gmail.com>
2024-06-03 11:11:38 +0100

    'protect' middleware added to the get individual tour details route. essentially a user needs to be logged in to view a tour

--- 4-natours/starter/routes/viewRoutes.js
+++ 4-natours/starter/routes/viewRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const viewsController = require('../controllers/viewsController');
+const authController = require('../controllers/authController');
 
 const routes = express.Router();
 
 routes.get('/', viewsController.getOverview);
-routes.get('/tour/:slug', viewsController.getTour);
+routes.get('/tour/:slug', authController.protect, viewsController.getTour);
 routes.get('/login', viewsController.getLoginForm)
 
 module.exports = routes;
