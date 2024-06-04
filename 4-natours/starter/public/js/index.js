c65c8e66ca2d29e29c71e2cc16d73086b3dcae8e
George Barrett <georgeaustinbarrett@gmail.com>
2024-06-04 16:46:01 +0100

    variable moved inside if (loginForm) statement. displayMap function imported and invoked. babel polyfill imported for the bundler. code tidied

--- 4-natours/starter/public/js/index.js
+++ 4-natours/starter/public/js/index.js
@@ -1,9 +1,21 @@
-import { login } from "./login";
+/* eslint-disabled */
 
-const loginForm = document.querySelector('.form--login');
+import '@babel/polyfill';
+import { login } from "./login";
+import { displayMap } from './mapbox';
+
+// DOM elements - to ensure pages without .map do not throw errors
+const mapBox = document.getElementById('map');
+const loginForm = document.querySelector('.form');
+
+// DELEGATION
+if (mapBox) {
+    const locations = JSON.parse(mapBox.dataset.locations);
+    displayMap(locations);
+};
 
 if (loginForm) {
-    loginForm.addEventListener('submit', (e) => {
+    document.querySelector('.form').addEventListener('submit', e => {
         e.preventDefault();
         const email = document.getElementById('email').value;
         const password = document.getElementById('password').value;
