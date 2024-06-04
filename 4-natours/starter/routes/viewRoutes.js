67511c76acf5c60755d1ab3fa0b60708817184f4
George Barrett <georgeaustinbarrett@gmail.com>
2024-06-04 11:35:48 +0100

    all of the routes in this file use the isLoggedIn middleware function for every request that comes in

--- 4-natours/starter/routes/viewRoutes.js
+++ 4-natours/starter/routes/viewRoutes.js
@@ -4,8 +4,10 @@ const authController = require('../controllers/authController');
 
 const routes = express.Router();
 
+routes.use(authController.isLoggedIn);
+
 routes.get('/', viewsController.getOverview);
-routes.get('/tour/:slug', authController.protect, viewsController.getTour);
+routes.get('/tour/:slug', viewsController.getTour);
 routes.get('/login', viewsController.getLoginForm)
 
 module.exports = routes;
