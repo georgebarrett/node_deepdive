f4f3c56df9fd2cea3ebefbba0506ff1d6206e930
George Barrett <georgeaustinbarrett@gmail.com>
2024-06-05 16:44:50 +0100

    if statement to catch errors if a user enter a tour that doesn't exist into the browser

--- 4-natours/starter/controllers/viewsController.js
+++ 4-natours/starter/controllers/viewsController.js
@@ -1,4 +1,5 @@
 const Tour = require('../models/tourModel');
+const AppError = require('../utils/appError');
 const catchAsync = require('../utils/catchAsyncError');
 
 const getOverview = catchAsync(async (req, res, next) => {
@@ -16,7 +17,11 @@ const getTour = catchAsync(async (req, res, next) => {
     const tour = await Tour.findOne({ slug: req.params.slug }).populate({
         path: 'reviews',
         fields: 'review rating user'
-    })
+    });
+
+    if (!tour) {
+        return next(new AppError('There is no tour with that name.', 404));
+    }
     
     res.status(200).render('tour', {
         title: `${tour.name} Tour`,
