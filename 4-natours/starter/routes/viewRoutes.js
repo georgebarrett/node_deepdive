12e7a3cf01d2275de562be7a129ddc9f5e54305b
George Barrett <georgeaustinbarrett@gmail.com>
2024-06-06 11:44:53 +0100

    now users can only view individual tours once they are logged in. this is due to the authController.protect middleware

--- 4-natours/starter/routes/viewRoutes.js
+++ 4-natours/starter/routes/viewRoutes.js
@@ -7,7 +7,7 @@ const routes = express.Router();
 routes.use(authController.isLoggedIn);
 
 routes.get('/', viewsController.getOverview);
-routes.get('/tour/:slug', viewsController.getTour);
+routes.get('/tour/:slug', authController.protect, viewsController.getTour);
 routes.get('/login', viewsController.getLoginForm)
 
 module.exports = routes;
