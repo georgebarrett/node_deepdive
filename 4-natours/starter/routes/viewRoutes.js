a0d7f87abed905fbdf3bbea48677c6692884284f
George Barrett <georgeaustinbarrett@gmail.com>
2024-06-21 16:09:02 +0100

    new post route added for submitting user data. .isLoggedIn is now not applied to all routes due to .protect being more suitable for two

--- 4-natours/starter/routes/viewRoutes.js
+++ 4-natours/starter/routes/viewRoutes.js
@@ -5,8 +5,10 @@ const authController = require('../controllers/authController');
 const routes = express.Router();
 
 routes.get('/', authController.isLoggedIn, viewsController.getOverview);
-routes.get('/tour/:slug', authController.protect, viewsController.getTour);
+routes.get('/tour/:slug', authController.isLoggedIn, viewsController.getTour);
 routes.get('/login', authController.isLoggedIn, viewsController.getLoginForm);
 routes.get('/me', authController.protect, viewsController.getAccount);
+
+routes.post('/submit-user-data', authController.protect, viewsController.updateUserData);
 
 module.exports = routes;
