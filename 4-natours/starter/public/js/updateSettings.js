971367d501c6c40cd5a17bbf64dd8d4fe4a4c315
George Barrett <georgeaustinbarrett@gmail.com>
2024-06-26 16:16:14 +0100

    console logs removed

--- 4-natours/starter/public/js/updateSettings.js
+++ 4-natours/starter/public/js/updateSettings.js
@@ -6,12 +6,9 @@ import { showAlert } from './alerts';
 // type is either password or name & email
 export const updateSettings = async (data, type) => {
     try {
-        console.log('Data to be sent:', data);
         const url = type === 'password' 
             ? 'http://localhost:3000/api/v1/users/updateMyPassword' 
             : 'http://localhost:3000/api/v1/users/updateMe';
-
-        console.log('URL:', url);
 
         const res = await axios({
             method: 'PATCH',
@@ -19,14 +16,11 @@ export const updateSettings = async (data, type) => {
             data
         });
 
-        console.log('Response:', res);
-
         if (res.data.status === 'success') {
             showAlert('success', `${type.toUpperCase()} updated successfully`);
         }
 
     } catch (error) {
-        console.error('Error:', error);
         showAlert('error', error.response.data.message)
     }
 };
