ea797c93dc81126f55070c67605dd6b234a925c9
George Barrett <georgeaustinbarrett@gmail.com>
2024-06-26 16:33:10 +0100

    password input field reset to empty strings after password has been changed by the user

--- 4-natours/starter/public/js/index.js
+++ 4-natours/starter/public/js/index.js
@@ -5,59 +5,61 @@ import { login, logout } from "./login";
 import { displayMap } from './mapbox';
 import { updateSettings } from './updateSettings';
 
-document.addEventListener('DOMContentLoaded', () => {
-    // DOM elements - to ensure pages without .map do not throw errors
-    // const mapBox = document.getElementById('map');
-    const loginForm = document.querySelector('.form--login');
-    const logOutButton = document.querySelector('.nav__el--logout');
-    const userDataForm = document.querySelector('.form-user-data');
-    const userPasswordForm = document.querySelector('.form-user-password');
+// DOM elements - to ensure pages without .map do not throw errors
+// const mapBox = document.getElementById('map');
+const loginForm = document.querySelector('.form--login');
+const logOutButton = document.querySelector('.nav__el--logout');
+const userDataForm = document.querySelector('.form-user-data');
+const userPasswordForm = document.querySelector('.form-user-password');
 
-    // DELEGATION
-    // if (mapBox) {
-    //     const locations = JSON.parse(mapBox.dataset.locations);
-    //     displayMap(locations);
-    // };
+// DELEGATION
+// if (mapBox) {
+//     const locations = JSON.parse(mapBox.dataset.locations);
+//     displayMap(locations);
+// };
 
-    if (loginForm) {
-        loginForm.addEventListener('submit', e => {
-            e.preventDefault();
-            const email = document.getElementById('email').value;
-            const password = document.getElementById('password').value;
-            login(email, password);
-        });
-    };
+if (loginForm) {
+    loginForm.addEventListener('submit', e => {
+        e.preventDefault();
+        const email = document.getElementById('email').value;
+        const password = document.getElementById('password').value;
+        login(email, password);
+    });
+};
 
-    if (logOutButton) {
-        logOutButton.addEventListener('click', logout);
-    };
+if (logOutButton) {
+    logOutButton.addEventListener('click', logout);
+};
 
-    if (userDataForm) {
-        userDataForm.addEventListener('submit', e => {
-            e.preventDefault();
-            const name = document.getElementById('name').value;
-            const email = document.getElementById('email').value;
-            updateSettings({ name, email }, 'data');
-        });
-    };
+if (userDataForm) {
+    userDataForm.addEventListener('submit', e => {
+        e.preventDefault();
+        const name = document.getElementById('name').value;
+        const email = document.getElementById('email').value;
+        updateSettings({ name, email }, 'data');
+    });
+};
 
-    if (userPasswordForm) {
-        userPasswordForm.addEventListener('submit', e => {
-            e.preventDefault();
-            console.log('Password form submitted');
-            const passwordCurrent = document.getElementById('password-current').value;
-            const password = document.getElementById('password').value;
-            const passwordConfirmation = document.getElementById('password-confirm').value;
-            updateSettings({ passwordCurrent, password, passwordConfirmation }, 'password');
-        });
-    };
+if (userPasswordForm) {
+    userPasswordForm.addEventListener('submit', async e => {
+        e.preventDefault();
+        console.log('Password form submitted');
+        const passwordCurrent = document.getElementById('password-current').value;
+        const password = document.getElementById('password').value;
+        const passwordConfirmation = document.getElementById('password-confirm').value;
+        await updateSettings({ passwordCurrent, password, passwordConfirmation }, 'password');
 
-    // Prevent Chrome autofill
-    const passwordFields = document.querySelectorAll('input[type="password"]');
-    passwordFields.forEach(field => {
-        field.setAttribute('readonly', 'true');
-        field.addEventListener('focus', () => {
-            field.removeAttribute('readonly');
-        });
+        document.getElementById('password-current').value = '';
+        document.getElementById('password').value = '';
+        document.getElementById('password-confirm').value = '';
+    });
+};
+
+// Prevent Chrome autofill
+const passwordFields = document.querySelectorAll('input[type="password"]');
+passwordFields.forEach(field => {
+    field.setAttribute('readonly', 'true');
+    field.addEventListener('focus', () => {
+        field.removeAttribute('readonly');
     });
 });
