cb3825fa9ccc02cd75c595b9ab4fd361dee59980
George Barrett <georgeaustinbarrett@gmail.com>
2024-06-27 18:12:36 +0100

    event added to the 'save password' button. the button now displays 'uploading...' when the user submits a req to change their password. the logic behind this approach is due to the changing password process takes some time. the user needs to know that something happening

--- 4-natours/starter/public/js/index.js
+++ 4-natours/starter/public/js/index.js
@@ -43,11 +43,13 @@ if (userDataForm) {
 if (userPasswordForm) {
     userPasswordForm.addEventListener('submit', async e => {
         e.preventDefault();
+        document.querySelector('.btn--save-password').innerHTML = 'Updating...';
         const passwordCurrent = document.getElementById('password-current').value;
         const password = document.getElementById('password').value;
         const passwordConfirmation = document.getElementById('password-confirm').value;
         await updateSettings({ passwordCurrent, password, passwordConfirmation }, 'password');
 
+        document.querySelector('.btn--save-password').innerHTML = 'Save password';
         document.getElementById('password-current').value = '';
         document.getElementById('password').value = '';
         document.getElementById('password-confirm').value = '';
