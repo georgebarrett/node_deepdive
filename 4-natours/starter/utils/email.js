8a1c8ad8e20aa57fc5d3e17fa8445b91bc4e0c70
George Barrett <georgeaustinbarrett@gmail.com>
2024-08-01 16:51:37 +0100

    class Email is being constructed to handle the sending of emails to users. a class is being constructed becasue there will many instances of emails being sent to users. such was welcome, password changed, monthly newsletter etc

--- 4-natours/starter/utils/email.js
+++ 4-natours/starter/utils/email.js
@@ -1,15 +1,34 @@
 const nodemailer = require('nodemailer');
 
-const sendEmail = async (options) => {
-    
-    const transporter = nodemailer.createTransport({
-        host: process.env.EMAIL_HOST,
-        port: process.env.EMAIL_PORT,
-        auth: {
-            user: process.env.EMAIL_USERNAME,
-            pass: process.env.EMAIL_PASSWORD   
+class Email {
+    // the function that runs when a new object is created through this class
+    // the user and url are the bits needed to send emails. eg welcome emails when a user signsup
+    constructor(user, url) {
+        this.to = user.email;
+        // for personalising the email
+        this.firstName = user.name.split(' ')[0];
+        // equal to the incoming email
+        this.url = url;
+        this.from = `Aphex Twin ${process.env.EMAIL_FROM}`;
+    }
+
+    createTransport() {
+        if (process.env.NODE_ENV === 'production') {
+            // sendgrid
+            return 1;
         }
-    });
+
+        return nodemailer.createTransport({
+            host: process.env.EMAIL_HOST,
+            port: process.env.EMAIL_PORT,
+            auth: {
+                user: process.env.EMAIL_USERNAME,
+                pass: process.env.EMAIL_PASSWORD   
+            }
+        });
+    }
+}
+const sendEmail = async (options) => {
 
     const mailOptions = {
         from: 'Aphex Twin <george@george.com>',
