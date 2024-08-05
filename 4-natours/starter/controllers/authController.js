18c810bd55c00f10ce98e93e240da097e8d882f6
George Barrett <georgeaustinbarrett@gmail.com>
2024-08-05 14:51:06 +0100

    forgotPassword function refactored. it now creates an object from the Email class in email.js and then calls the sendPasswordReset method to send a forgot password email to Mailtrap. the email contains a url that enables the user to reset their password

--- 4-natours/starter/controllers/authController.js
+++ 4-natours/starter/controllers/authController.js
@@ -162,16 +162,10 @@ const forgotPassword = catchAsyncErrors(async (req, res, next) => {
     const resetToken = user.createPasswordResetToken();
     await user.save({ validateBeforeSave: false });
 
-    const resetURL = `${req.protocol}://${req.get('host')}/api/v1/users/resetPassword/${resetToken}`;
-
-    const message = `Forgot your password? Submit a PATCH request with your new password to: ${resetURL}. If you didn't forget your password, please ignore this email.`;
-
     try {
-        // await sendEmail({
-        //     email: user.email,
-        //     subject: 'Your password reset token (valid for 10 minutes)',
-        //     message: message,
-        // });
+        const resetURL = `${req.protocol}://${req.get('host')}/api/v1/users/resetPassword/${resetToken}`;
+        
+        await new Email(user, resetURL).sendPasswordReset();
 
         res.status(200).json({
             status: 'success',
