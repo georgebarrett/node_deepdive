82281f6e82abc4b4b8912990e5a9d5779b7660b6
George Barrett <georgeaustinbarrett@gmail.com>
2024-08-06 17:55:24 +0100

    unwanted import removed

--- 4-natours/starter/routes/bookingRoutes.js
+++ 4-natours/starter/routes/bookingRoutes.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const bookingController = require('../controllers/bookingController');
 const authController = require('../controllers/authController');
-const crudFactory = require('../controllers/crudFactory');
 
 const routes = express.Router();
 
