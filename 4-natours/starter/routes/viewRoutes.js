cb8f032c19e5fa6c4d40954106862c2ff8de7257
George Barrett <georgeaustinbarrett@gmail.com>
2024-08-07 15:09:34 +0100

    createBookingCheckout middleware function added to booking route. find the logic of this function in bookingController.js. it essentially ensures users can't book tours without paying by protecting the URL

--- 4-natours/starter/routes/viewRoutes.js
+++ 4-natours/starter/routes/viewRoutes.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const viewsController = require('../controllers/viewsController');
 const authController = require('../controllers/authController');
+const bookingController = require('../controllers/bookingController');
 
 const routes = express.Router();
 
-routes.get('/', authController.isLoggedIn, viewsController.getOverview);
+// home page is hit, a new booking is created, the user is then redirected to the home page but without the sensitive url params
+// then the user passes through the isLoggedIn middleware and reaches the getOverview function
+routes.get('/', bookingController.createBookingCheckout, authController.isLoggedIn, viewsController.getOverview);
 routes.get('/tour/:slug', authController.isLoggedIn, viewsController.getTour);
 routes.get('/login', authController.isLoggedIn, viewsController.getLoginForm);
 routes.get('/me', authController.protect, viewsController.getAccount);
